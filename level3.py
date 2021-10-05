102e8492e73d65fc93aa9f23d2b0b2e2b3747655
Ashok-gc <78782373+Ashok-gc@users.noreply.github.com>
2021-10-05 20:05:42 +0545

    lev 2

--- level3.py
+++ level3.py
@@ -511,12 +511,12 @@ class Bullet(pygame.sprite.Sprite):
         # check collision with characters
         if pygame.sprite.spritecollide(player, bullet_group, False):
             if player.alive:
-                player.health -= 15
+                player.health -= 10
                 self.kill()
         for enemy in enemy_group:
             if pygame.sprite.spritecollide(enemy, bullet_group, False):
                 if enemy.alive:
-                    enemy.health -= 30
+                    enemy.health -= 40
                     self.kill()
 
 
